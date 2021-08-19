9edc239043db309d851b3734d1360be952f2ea29
Neeraj Kumar <neerajn@policybazaar.com>
2021-08-19 16:52:37 +0530

    Updates

---
+++ precache-manifest.57f3f324afcd4aed47cda9f9e7bdcfbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf8cb2b9ccb79426dfa58d2de3caecf",
+    "url": "/react-web-prac/index.html"
+  },
+  {
+    "revision": "a829cb2eb3f8115d44d3",
+    "url": "/react-web-prac/static/css/main.bc47aaed.chunk.css"
+  },
+  {
+    "revision": "c5b4d0c9ccc1af6305c0",
+    "url": "/react-web-prac/static/js/2.ea58a1b5.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/react-web-prac/static/js/2.ea58a1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a829cb2eb3f8115d44d3",
+    "url": "/react-web-prac/static/js/main.17ef6d41.chunk.js"
+  },
+  {
+    "revision": "e236a2b165fe7a27011d",
+    "url": "/react-web-prac/static/js/runtime-main.c32223d2.js"
+  }
+]);
